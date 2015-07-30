6da0e11da806965afd9d4e8a45af549e335654b6
Ken Youens-Clark <kyclark@gmail.com>
2015-07-30 15:08:00 -0700

    option to get from input group/distributor file, back to using Perl until I fix Haskell

--- pbs/run-binner.sh
+++ pbs/run-binner.sh
@@ -3,7 +3,6 @@
 #PBS -q standard
 #PBS -l jobtype=cluster_only
 #PBS -l select=1:ncpus=4:mem=10gb
-#PBS -l pvmem=20gb
 #PBS -l walltime=24:00:00
 #PBS -l cput=24:00:00
 
@@ -17,9 +16,13 @@ fi
 
 TMP_FILES=$(mktemp)
 
-get_lines $FILES_LIST $TMP_FILES ${PBS_ARRAY_INDEX:=1} ${STEP_SIZE:=1}
-
-cat $TMP_FILES
+if [[ ${INPUT_GROUP_FILE:=""} != "" ]] && [[ ${PBS_ARRAY_INDEX:=1} -gt 0 ]]
+then
+  awk -F"\t" "\$1 == $PBS_ARRAY_INDEX {print \$2}" $INPUT_GROUP_FILE \
+    > $TMP_FILES
+else
+  get_lines $FILES_LIST $TMP_FILES ${PBS_ARRAY_INDEX:=1} ${STEP_SIZE:=1}
+fi
 
 NUM_FILES=$(lc $TMP_FILES)
 
@@ -35,14 +38,20 @@ echo Processing \"$NUM_FILES\" files
 
 i=0
 while read FILE; do
+  BASENAME=$(basename $FILE)
+  SIZE=$(ls -lh $FILE | awk '{print $5}')
+
   let i++
+  printf "%5d: %s (%s)\n" $i $BASENAME $SIZE
 
-  printf "%5d: %s\n" $i $(basename $FILE)
+  DIR=$OUT_DIR/$BASENAME
 
-  BASE_DIR=$(dirname $FILE)
-  BASE_DIR=$(basename $BASE_DIR)
+  if [[ ! -d $DIR ]]; then
+    mkdir -p $DIR
+    time jellyfish dump -c $FILE | $SCRIPTS/binner.pl -o $DIR
+  fi
 
-  time $SCRIPTS/binner.pl -q -f $FILE -o "$OUT_DIR/$BASE_DIR"
+  #time $SCRIPTS/kmer-binner -f $FILE -o "$OUT_DIR/$BASE_DIR"
 done < $TMP_FILES
 
 rm $TMP_FILES
